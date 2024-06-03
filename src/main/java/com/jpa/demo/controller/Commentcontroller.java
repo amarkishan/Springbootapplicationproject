b9a15aebf74c1172b9cba58ccb38834be7707352
amarkishan <amar6333kartheek@gmail.com>
2024-06-03 13:03:13 -0400

    changes made to the pom

--- src/main/java/com/jpa/demo/controller/Commentcontroller.java
+++ src/main/java/com/jpa/demo/controller/Commentcontroller.java
@@ -2,7 +2,6 @@ package com.jpa.demo.controller;
 
 import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,35 +11,57 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.jpa.demo.entity.Comments;
 import com.jpa.demo.entity.Projectinfo;
 import com.jpa.demo.entity.Taskinfo;
 import com.jpa.demo.repository.CommentRepository;
 import com.jpa.demo.repository.TaskinfoRepository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @RestController
 @RequestMapping("/api/comments")
 public class Commentcontroller 
 {
 
-//	@Autowired
-//	private CommentRepository commentrepository;
+	 private static final Logger logger = LoggerFactory.getLogger(Commentcontroller.class);
+	@Autowired
+	private CommentRepository commentrepository;
 	@Autowired
     private TaskinfoRepository taskinfoRepository;
 	 
 	 //this will return the comment associated with the taskid
-	 @GetMapping("/task/{taskid}")
-	 public  List<Comments> readcommentsfortask(@PathVariable int taskid)
+	 @GetMapping("/task/{commentid}")
+	 public    Taskinfo readcommentsfortask(@PathVariable int commentid)
 	 {
 		 
-		 Optional<Taskinfo> taskinfoOptional = taskinfoRepository.findById(taskid);
-		 Taskinfo tf1 = taskinfoOptional.get();
-		 List<Comments> c = tf1.getComments();
+		   
+
+	
+            logger.info("Received request to get commnets for task with commentid:{}",commentid);
+			Optional<Comments> commentoptional = commentrepository.findById(commentid);
+			if(commentoptional.isPresent())
+			{
+				Comments c = commentoptional.get();
+			    logger.debug("Found commnet:{}",c);
+			    return c.getTaskinfo();
+			    
+			}
+			
+			else
+			{
+				logger.warn("comment with id {} not found",commentid);
+				throw new RuntimeException("comment not foound");
+				
+			}
+
+			
+			
+			
+			
 		 
-		 return c;
-		 
-		 
-		 
-	 }
+        }
+
+	
 }
--- src/test/java/com/jpa/demo/Commenttest.java
+++ src/test/java/com/jpa/demo/Commenttest.java
@@ -1,13 +1,25 @@
 package com.jpa.demo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
+
 
 import com.jpa.demo.controller.Commentcontroller;
-import com.jpa.demo.controller.Taskcontroller;
+
+import com.jpa.demo.entity.Comments;
+import com.jpa.demo.entity.Taskinfo;
 import com.jpa.demo.repository.CommentRepository;
-import com.jpa.demo.repository.TaskinfoRepository;
+
 
 public class Commenttest 
 {
@@ -16,9 +28,40 @@ public class Commenttest
 	
 	@InjectMocks
 	Commentcontroller commnetcontrollermock; 
-    @Mock
-    private TaskinfoRepository taskinfoRepository;
     
+	@Mock
+	
+	private CommentRepository commentrepository;
     
+	@Test
+    public void testReadcommentsfortaskCommentNotFound() 
+	{
+        // Given
+        int commentid = 1;
+        
+        when(commentrepository.findById(commentid)).thenReturn(Optional.empty());
+
+        // When / Then
+        RuntimeException exception = assertThrows(RuntimeException.class, () -> {
+        	commnetcontrollermock.readcommentsfortask(commentid);
+        });
+        
+        assertEquals("comment not foound", exception.getMessage());
+        verify(commentrepository, times(1)).findById(commentid);
+    }
 	 
+	@Test
+    public void testReadcommentsfortaskCommentFound() {
+        // Given
+        int commentid = 1;
+        Comments comment = new Comments();
+        Taskinfo taskinfo = new Taskinfo();
+        comment.setTaskinfo(taskinfo);
+        
+        when(commentrepository.findById(commentid)).thenReturn(Optional.of(comment));
+        Taskinfo result = commnetcontrollermock.readcommentsfortask(commentid);
+        assertNotNull(result);
+        assertEquals(taskinfo, result);
+        verify(commentrepository, times(1)).findById(commentid);
+    }
 }
